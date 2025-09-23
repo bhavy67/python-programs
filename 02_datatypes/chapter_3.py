4e58f24d959544ef17a5bf653283e57f704cd74d
bhavy67 <bhavy.ladani6701@gmail.com>
2025-09-23 10:01:11 +0530

    Numbers and Integers

---
+++ 02_datatypes/chapter_3.py
@@ -0,0 +1,22 @@
+black_tea_gram = 5
+ginger_tea_gram = 7
+total_tea_gram = black_tea_gram + ginger_tea_gram
+print(f"Total Tea: {total_tea_gram} grams")
+
+remaining_tea_gram = black_tea_gram - ginger_tea_gram
+print(f"Remaining Tea: {remaining_tea_gram} grams")
+
+milk_liters = 7
+serving = 4
+milk_per_serving = milk_liters / serving
+print(f"Milk per Serving: {milk_per_serving} liters")
+
+total_tea_bags = 7
+tea_bags_per_person = 4
+people_served = total_tea_bags // tea_bags_per_person
+print(f"People Served: {people_served}")
+
+total_cardamom_pods = 7
+pods_per_tea = 4
+remaining_pods = total_cardamom_pods % pods_per_tea
+print(f"Remaining Cardamom Pods: {remaining_pods}")
