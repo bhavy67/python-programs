f470e2ccefc65fbcb2c495861a6565cfc0156381
bhavy67 <bhavy.ladani6701@gmail.com>
2025-10-06 07:04:26 +0530

    functions 2

---
+++ 05_functions/02_complex.py
@@ -0,0 +1,14 @@
+def fetch_sales():
+    print("Fetching sales data...")
+
+def filter_valid_sales():
+    print("Filtering valid sales...")
+
+def summarize_sales():
+    print("Summarizing sales data...")
+
+def generate_report():
+    fetch_sales()
+    filter_valid_sales()
+    summarize_sales()
+    print("Generating sales report...")
