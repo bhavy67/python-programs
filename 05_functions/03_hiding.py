0f3392f7a6d0df6ee26698b7d50a509b0379bc27
bhavy67 <bhavy.ladani6701@gmail.com>
2025-10-06 09:50:49 +0530

    functions files added

---
+++ 05_functions/03_hiding.py
@@ -0,0 +1,16 @@
+def get_input():
+    print("Getting user input...")
+
+def validate_input():
+    print("Validating user input...")
+
+def save_to_database():
+    print("Saving data to database...")
+
+def register_user():
+    get_input()
+    validate_input()
+    save_to_database()
+    print("User registration complete.")
+
+register_user()
---
+++ 05_functions/04_readibility.py
@@ -0,0 +1,7 @@
+def calculate_bill(cups, price_per_cup):
+    return cups * price_per_cup
+
+
+my_bill = calculate_bill(3, 5)
+print(f"Your bill is ${my_bill}")
+
