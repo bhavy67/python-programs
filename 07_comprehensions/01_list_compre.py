d437d4f321c169fb7a22ec1938274b3c47f57f53
bhavy67 <bhavy.ladani6701@gmail.com>
2025-10-08 22:25:29 +0530

    list compre

---
+++ 07_comprehensions/01_list_compre.py
@@ -0,0 +1,12 @@
+menu = [
+    "Iced Coffee",
+    "Latte",
+    "Iced Latte",
+    "Cappuccino",
+    "Espresso",
+    "Straweberry Iced Cream"
+]
+
+iced_teas = [beverage for beverage in menu if "Iced" in beverage]
+print(iced_teas)
+
