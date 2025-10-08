baa501612062fa339922d20bd5faece9b8a79972
bhavy67 <bhavy.ladani6701@gmail.com>
2025-10-08 22:28:08 +0530

    set compre

---
+++ 07_comprehensions/02_set_compre.py
@@ -0,0 +1,12 @@
+favourite_chais =[
+    "Masala Chai",
+    "Ginger Chai",
+    "Cardamom Chai",
+    "Lemon Chai",
+    "Green Tea",
+    "Black Tea",
+    "Masala Chai",
+]
+
+unique_chais = {chai for chai in favourite_chais}
+print(unique_chais)
