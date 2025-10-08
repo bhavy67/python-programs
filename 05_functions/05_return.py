e1036c92fe6e702cd8d9423c299de018d182461d
bhavy67 <bhavy.ladani6701@gmail.com>
2025-10-08 08:46:20 +0530

    return function

---
+++ 05_functions/05_return.py
@@ -0,0 +1,6 @@
+def make_chai(tea_type, milk, sugar):
+    chai = f"Making {tea_type} chai with {milk} and {sugar}."
+    return chai
+
+my_chai = make_chai("Masala", "whole milk", "2 tsp sugar")
+print(my_chai)
