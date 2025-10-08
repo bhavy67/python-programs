d449378bb591199988f3f2b6dff4bf9d6b04b552
bhavy67 <bhavy.ladani6701@gmail.com>
2025-10-08 22:19:31 +0530

    builtin

---
+++ 05_functions/07_builtin.py
@@ -0,0 +1,8 @@
+def chai_flavor(flavor="Masala"):
+    """Returns a string describing the chai flavor."""
+    chai='ginger'
+    return f"Chai flavor is {flavor}"
+
+
+print(chai_flavor.__doc__)
+print(chai_flavor.__name__)
