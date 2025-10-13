09aad8addf96500a02b5098a124897f6e18fa184
bhavy67 <bhavy.ladani6701@gmail.com>
2025-10-13 22:20:27 +0530

    simple.py

---
+++ 10_oops/01_simple.py
@@ -0,0 +1,12 @@
+class Chai:
+    pass
+
+class Chai_time:
+    pass    
+
+print(type(Chai))
+
+ginger_tea = Chai()
+print(type(ginger_tea))
+print(type(ginger_tea) is Chai)
+print(type(ginger_tea) is Chai_time)
