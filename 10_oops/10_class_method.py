3e55a952dc2110f8b4853081dc9d23d6b8c508a4
bhavy67 <bhavy.ladani6701@gmail.com>
2025-10-19 23:42:57 +0530

    class method

---
+++ 10_oops/10_class_method.py
@@ -0,0 +1,19 @@
+class Chaiorder:
+
+    def __init__(self, tea_type, sugar_level, size):
+        self.tea_type = tea_type
+        self.sugar_level = sugar_level
+        self.size = size
+
+    @classmethod
+    def from_string(cls, order_string):
+        tea_type, sugar_level, size = order_string.split('-')
+        return cls(tea_type, sugar_level, size)
+    
+    @classmethod
+    def from_dict(cls, order_dict):
+        return cls(
+            order_dict['tea_type'],
+            order_dict['sugar_level'],
+            order_dict['size']
+        )
